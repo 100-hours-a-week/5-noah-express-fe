bba0e5fa277e3de2cc397b6f62f4f06d3ad78b2b
49EHyeon42 <49ehyeon42@gmail.com>
2024-04-29 22:45:16 +0900

    squash-merge: 2주차 2번 과제 마무리
    commit eca99027e97f692ddaa6723c1a278a53522618be
    Author: 49EHyeon42 <49ehyeon42@gmail.com>
    Date:   Mon Apr 29 22:01:42 2024 +0900
    
        fix: 게시물을 수저할 수 있도록 수정
    
    commit ae93aa0e4d60d20272c0db9203bdae9e7abb9bfd
    Author: 49EHyeon42 <49ehyeon42@gmail.com>
    Date:   Mon Apr 29 21:32:36 2024 +0900
    
        fix: 댓글을 삭제할 수 있도록 수정
    
    commit 47bbd5983234e291b44a9ed13d27c85cb31bb5e5
    Author: 49EHyeon42 <49ehyeon42@gmail.com>
    Date:   Mon Apr 29 21:16:09 2024 +0900
    
        fix: 댓글을 작성할 수 있도록 수정
    
    commit e461eacb9fd8b1663cdae762b09b1de3e7384187
    Author: 49EHyeon42 <49ehyeon42@gmail.com>
    Date:   Mon Apr 29 14:58:25 2024 +0900
    
        feat: 게시글 삭제 구현
    
    commit 86a7f054ea59db5289c71002c7a46f3da03640d2
    Author: 49EHyeon42 <49ehyeon42@gmail.com>
    Date:   Mon Apr 29 14:54:07 2024 +0900
    
        fix: 게시글 작성 중 이미지가 보내지지 않는 문제 발생, 수정
    
    commit 1a13e410b3ef45d9885ab745b76fe3f75e3acad7
    Author: 49EHyeon42 <49ehyeon42@gmail.com>
    Date:   Mon Apr 29 13:35:17 2024 +0900
    
        fix: post 중 게시글 상세와 댓글을 조회할 수 있도록 변경
    
    commit 00fd4e4f3e289805929041eb0b205c8804ad8d02
    Author: 49EHyeon42 <49ehyeon42@gmail.com>
    Date:   Mon Apr 29 13:24:49 2024 +0900
    
        fix: edit-post 중 게시글을 작서할 수 있도록 변경
    
    commit 13df153c0ec2708019712e7cf543a1de6f07ec44
    Author: 49EHyeon42 <49ehyeon42@gmail.com>
    Date:   Mon Apr 29 13:12:42 2024 +0900
    
        fix: posts 중 fetch를 json 대신 api로 변경

--- public/update-post.js
+++ public/update-post.js
@@ -1,3 +1,7 @@
+// init
+const SERVER_ADDRESS = 'http://localhost';
+const SERVER_PORT = '8000';
+
 const postId = parseInt(window.location.href.match(/\/(\d+)$/)[1]);
 
 // status
@@ -8,71 +12,93 @@ const helperText = document.querySelector('.helper-text');
 const updatePostButton = document.getElementById('update-post-button');
 
 function changeHelpTextAndButtonColor() {
-  if (validTitleStatus && validContentStatus) {
-    helperText.innerText = '';
+    if (validTitleStatus && validContentStatus) {
+        helperText.innerText = '';
 
-    updatePostButton.style.backgroundColor = '#7F6AEE';
-  } else {
-    helperText.innerText = '* 제목, 내용을 모두 작성해주세요.';
+        updatePostButton.style.backgroundColor = '#7F6AEE';
+    } else {
+        helperText.innerText = '* 제목, 내용을 모두 작성해주세요.';
 
-    updatePostButton.style.backgroundColor = '#ACA0EB';
-  }
+        updatePostButton.style.backgroundColor = '#ACA0EB';
+    }
 }
 
 // 뒤로가기 버튼 로직
 document.querySelector('.move-page-button').addEventListener('click', () => {
-  window.location.href = '/posts';
+    window.location.href = '/posts';
 });
 
 // 드롭다운 메뉴 관련 로직
 const profileImageDropdown = document.querySelector('.profile-image-dropdown');
 
 document.querySelector('.profile-image').addEventListener('click', () => {
-  profileImageDropdown.classList.toggle('show');
+    profileImageDropdown.classList.toggle('show');
 });
 
 // 제목 관련 로직
 const titleInput = document.getElementById('title');
 
 titleInput.addEventListener('change', () => {
-  const value = titleInput.value.trim();
+    const value = titleInput.value.trim();
 
-  validTitleStatus = value.length != 0;
+    validTitleStatus = value.length != 0;
 
-  changeHelpTextAndButtonColor();
+    changeHelpTextAndButtonColor();
 });
 
 // 내용 관련 로직
 const contentInput = document.getElementById('content');
 
 contentInput.addEventListener('change', () => {
-  const value = contentInput.value.trim();
+    const value = contentInput.value.trim();
 
-  validContentStatus = value.length != 0;
+    validContentStatus = value.length != 0;
 
-  changeHelpTextAndButtonColor();
+    changeHelpTextAndButtonColor();
 });
 
-fetch('/json/posts.json')
-  .then((response) => {
-    if (!response.ok) {
-      throw new Error();
-    }
+const updatePostForm = document.getElementById('update-post-form');
 
-    return response.json();
-  })
-  .then((json) => {
-    const foundPost = json.posts.find(post => post.id === postId);
+updatePostForm.addEventListener('submit', (event) => {
+    event.preventDefault();
 
-    if (!foundPost) {
-      throw new Error();
-    }
+    const formDate = new FormData(updatePostForm);
+    formDate.set('content', contentInput.value);
 
-    titleInput.value = foundPost.title;
-    contentInput.value = foundPost.content;
-  })
-  .catch(error => {
-    alert(error.message);
+    fetch(`${SERVER_ADDRESS}:${SERVER_PORT}/api/posts/${postId}`, {
+        method: 'PATCH', body: formDate,
+    }).then((response) => {
+        if (!response.ok) {
+            throw new Error('게시물 수정 실패');
+        }
 
-    console.error(error);
-  });
+        window.location.href = `/posts/${postId}`;
+    }).catch((error) => {
+        alert(error);
+
+        console.error(error);
+
+        window.location.href = '/posts';
+    });
+});
+
+
+fetch(`${SERVER_ADDRESS}:${SERVER_PORT}/api/posts/${postId}`)
+    .then((response) => {
+        if (!response.ok) {
+            throw new Error('게시글 불러오기 실패');
+        }
+
+        return response.json();
+    })
+    .then((post) => {
+        titleInput.value = post.title;
+        contentInput.value = post.content;
+    })
+    .catch(error => {
+        alert(error.message);
+
+        console.error(error);
+
+        window.location.href = '/posts';
+    });
